ca1b8efac34ae19a8bab7d80f336d80aca32221f
Athmane Naciri <anaciri@e1r4p7.1337.ma>
2021-12-18 04:11:23 +0100

    i love you

---
+++ hafssa/ft_printf.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anaciri <anaciri@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/16 13:19:04 by anaciri           #+#    #+#             */
+/*   Updated: 2021/12/18 04:08:16 by anaciri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h" 
+
+int ft_putexp(char sp, va_list args)
+{	
+	if (sp == 'c')
+		return(ft_print_char(va_arg(args, int)));
+	if (sp == 's')
+		return (ft_print_str(va_arg(args, char *)));
+	else if (sp == 'd' || sp == 'i')
+		return (ft_print_nbr(va_arg(args, int)));
+	else if (sp == 'x' || sp == 'X')
+		return (ft_print_hexa(va_arg(args, unsigned int), sp));
+	else if (sp == 'u')
+		return (ft_print_unbr(va_arg(args, unsigned int)));
+	else if (sp == 'p')
+		return (ft_print_addr(va_arg(args, unsigned long long)));
+	else
+		return write(1, &sp, 1);
+}
+int ft_printf(const char *format, ...)
+{
+	va_list	args;
+	int i;
+	i = 0;
+	int counter = 0;
+	va_start(args, format);
+
+	while(format[i])
+	{
+		if(format[i]  == '%')
+		{
+			i++;
+			counter += ft_putexp(format[i], args);
+		}
+		else
+			counter += write(1, &format[i], 1);
+		i++;
+	}
+	return(counter);
+}
